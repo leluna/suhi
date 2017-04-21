58a2221afc1c240aaae9ce564eb9501191e9e7a1
Azel4231 <Azel4231@users.noreply.github.com>
2017-04-21 15:01:31 +0200

    First reagent component

--- suhitetris/src/suhitetris/core.cljs
+++ suhitetris/src/suhitetris/core.cljs
@@ -9,11 +9,24 @@
 
 (defonce app-state (atom {:text "Blabla"}))
 
+
+(defn tetris-component []
+  [:div
+   [:p "I am a component!"]
+  ["" [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+;; in case I need to call this from JS
+(defn ^:export run []
+  (r/render [tetris-component]
+            (js/document.getElementById "app")))
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-
+  (run)
+)+
+   [:p "I am a component!"]
   
-)
 
