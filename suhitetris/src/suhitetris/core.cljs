829b1a9f53219d789316bb3ed98b90be449f31df
leluna <jhelen90@googlemail.com>
2017-05-14 23:32:25 +0200

    add keys; add keypress binding

--- suhitetris/src/suhitetris/core.cljs
+++ suhitetris/src/suhitetris/core.cljs
@@ -31,36 +31,48 @@
                             :block {:position [5 0]}
                             :settings {:gravity true}}))
 
-(defn inc-y [[v1 v2]]
+(defn move-down [[v1 v2]]
   [(min (inc v1) 21) v2])
 
+(defn move-right [[v1 v2]]
+  [ v1 (min (inc v2) 9)])
+
+(defn move-left [[v1 v2]]
+  [v1 (max (inc v2) 0)])
+
 (defn drop-block [state]
-  (update-in state [:block :position] inc-y))
+  (update-in state [:block :position] move-down))
 
 
 (defonce tick! (js/setInterval
                 #(when (get-in @app-state [:settings :gravity]) (swap! app-state drop-block)) 750))
 
-
 (defn merge-block [graveyard block]
   (assoc-in graveyard (:position block) 1))
   
 
+
+(defn line-of-blocks [index values]
+  ^{:key index}[:div.line]
+    (map-indexed (fn [i v]
+                   ^{:key i}[:div.cell (cond (= v 0) {:class "dead"}                                          
+                                             (= v 1) {:class "alife"}
+                                             :else   {:class "empty"})])
+      values))
+
 (defn board []
-  [:div
+  [:div.game
    [:div.container 
-    [:div 
-     (for [line (merge-block (:board @app-state) (:block @app-state))]
-       [:div.line (for [cell line]
-                    [:div.cell (cond (= cell 0) {:class "dead"}     
-                                     (= cell 1) {:class "alife"}
-                                     :else      {:class "empty"})])])]]
+    [:div.board
+     (map-indexed line-of-blocks (merge-block (:board @app-state) (:block @app-state)))]]
+    
    [:input {:type :checkbox
             :name "Gravity"
             :defaultChecked true
             :on-click #(swap! app-state update-in [:settings :gravity] not)}]
    [:div (str @app-state)]]) 
-   
+
+
 
 (defn mount-root []
   (r/render-component [board] 
@@ -69,4 +81,16 @@
 (defn on-js-reload [] 
   (mount-root))
 
+
+
+(defn handle-arrow-keys [event]
+  (let [key-code (.-keyCode event)]
+    (cond (= key-code 37) (println "left arrow key pressed")
+          (= key-code 38) (println "up arrow key pressed")
+          (= key-code 39) (println "right arrow key pressed")
+          (= key-code 40) (println "down arrow key pressed"))))
+
+(.addEventListener js/document "keydown" handle-arrow-keys)
+
 (mount-root)
+
