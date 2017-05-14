5a196476387cc39a371f56c49cade75ecfe3b54c
Azel4231 <Azel4231@users.noreply.github.com>
2017-05-14 20:05:39 +0200

    Gravity checkbox + app-state div for debuggin purposes

--- suhitetris/src/suhitetris/core.cljs
+++ suhitetris/src/suhitetris/core.cljs
@@ -28,7 +28,8 @@
                                     [nil nil nil nil nil nil nil nil nil nil]
                                     [nil nil nil nil nil nil nil nil nil nil]
                                     [nil nil nil nil nil nil nil nil 0 0]]
-                            :block {:position [5 0]}}))
+                            :block {:position [5 0]}
+                            :settings {:gravity true}}))
 
 (defn inc-y [[v1 v2]]
   [(min (inc v1) 21) v2])
@@ -37,7 +38,8 @@
   (update-in state [:block :position] inc-y))
 
 
-(defonce tick! (js/setInterval #(swap! app-state drop-block) 500))
+(defonce tick! (js/setInterval
+                #(when (get-in @app-state [:settings :gravity]) (swap! app-state drop-block)) 750))
 
 
 (defn merge-block [graveyard block]
@@ -45,13 +47,19 @@
   
 
 (defn board []
-  [:div.container 
-   [:div 
-    (for [line (merge-block (:board @app-state) (:block @app-state))]
-      [:div.line (for [cell line]
-                   [:div.cell (cond (= cell 0) {:class "dead"}     
-                                (= cell 1) {:class "alife"}
-                                :else      {:class "empty"})])])]]) 
+  [:div
+   [:div.container 
+    [:div 
+     (for [line (merge-block (:board @app-state) (:block @app-state))]
+       [:div.line (for [cell line]
+                    [:div.cell (cond (= cell 0) {:class "dead"}     
+                                     (= cell 1) {:class "alife"}
+                                     :else      {:class "empty"})])])]]
+   [:input {:type :checkbox
+            :name "Gravity"
+            :defaultChecked true
+            :on-click #(swap! app-state update-in [:settings :gravity] not)}]
+   [:div (str @app-state)]]) 
    
 
 (defn mount-root []
