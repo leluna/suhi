8fb3a37a006a8d1f932ca26f100848146ffa3f5e
leluna <jhelen90@googlemail.com>
2017-05-15 00:48:40 +0200

    add left right movement

--- suhitetris/src/suhitetris/core.cljs
+++ suhitetris/src/suhitetris/core.cljs
@@ -6,57 +6,80 @@
 (def pieces {})
 
 
-(defonce app-state (r/atom {:board [[nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil nil nil]
-                                    [nil nil nil nil nil nil nil nil 0 0]]
-                            :block {:position [5 0]}
+(defonce app-state (r/atom {:board [[0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 0 0]
+                                    [0 0 0 0 0 0 0 0 1 1]]
+                            :block {:position [5 3]}
                             :settings {:gravity true}}))
 
-(defn move-down [[v1 v2]]
-  [(min (inc v1) 21) v2])
+(defn valid-move? [board to]
+  (= (get-in board to) 0))
 
-(defn move-right [[v1 v2]]
-  [ v1 (min (inc v2) 9)])
+(defn down [[v1 v2]]
+  [(inc v1) v2])
 
-(defn move-left [[v1 v2]]
-  [v1 (max (inc v2) 0)])
+(defn right [[v1 v2]]
+  [v1 (inc v2)])
 
-(defn drop-block [state]
-  (update-in state [:block :position] move-down))
+(defn left [[v1 v2]]
+  [v1 (dec v2)])
+
+;; todo: merge to one function...
+(defn move-down [state]
+  (let [to (down (get-in state [:block :position]))]
+    (if (valid-move? (:board state) to)
+      (assoc-in state [:block :position] to)
+      state)))
+    
+
+(defn move-left [state]
+  (let [to (left (get-in state [:block :position]))]
+    (if (valid-move? (:board state) to)
+      (assoc-in state [:block :position] to)
+      state)))
+
+(defn move-right [state]
+  (let [to (right (get-in state [:block :position]))]
+    (if (valid-move? (:board state) to)
+      (assoc-in state [:block :position] to)
+      state)))
 
 
 (defonce tick! (js/setInterval
-                #(when (get-in @app-state [:settings :gravity]) (swap! app-state drop-block)) 750))
+                #(when (get-in @app-state [:settings :gravity]) (swap! app-state move-down)) 750))
+
+
+
 
 (defn merge-block [graveyard block]
-  (assoc-in graveyard (:position block) 1))
+  (assoc-in graveyard (:position block) 2))
   
 
 
 (defn line-of-blocks [index values]
   ^{:key index}[:div.line]
     (map-indexed (fn [i v]
-                   ^{:key i}[:div.cell (cond (= v 0) {:class "dead"}                                          
-                                             (= v 1) {:class "alife"}
+                   ^{:key i}[:div.cell (cond (= v 1) {:class "dead"}                                          
+                                             (= v 2) {:class "alife"}
                                              :else   {:class "empty"})])
       values))
 
@@ -83,14 +106,14 @@
 
 
 
-(defn handle-arrow-keys [event]
+(defn handle-arrow-keys! [event]
   (let [key-code (.-keyCode event)]
-    (cond (= key-code 37) (println "left arrow key pressed")
+    (cond (= key-code 37) (swap! app-state move-left)
           (= key-code 38) (println "up arrow key pressed")
-          (= key-code 39) (println "right arrow key pressed")
+          (= key-code 39) (swap! app-state move-right)
           (= key-code 40) (println "down arrow key pressed"))))
 
-(.addEventListener js/document "keydown" handle-arrow-keys)
+(.addEventListener js/document "keydown" handle-arrow-keys!)
 
 (mount-root)
 
