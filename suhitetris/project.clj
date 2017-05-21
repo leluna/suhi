926f74344df8ca161883a52fe941989ed44c535e
leluna <jhelen90@googlemail.com>
2017-05-22 00:41:38 +0200

    push changes to docs

--- suhitetris/project.clj
+++ suhitetris/project.clj
@@ -6,8 +6,8 @@
 
   :min-lein-version "2.7.1"
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.9.495" :scope "provided"]
                  [org.clojure/core.async  "0.3.442"
                   :exclusions [org.clojure/tools.reader]]
                  [reagent "0.6.1"]]
@@ -44,7 +44,7 @@
                ;; lein cljsbuild once min
                {:id "min"
                 :source-paths ["src"]
-                :compiler {:output-to "resources/public/js/compiled/suhitetris.js"
+                :compiler {:output-to "../docs/js/compiled/suhitetris.js"
                            :main suhitetris.core
                            :optimizations :advanced
                            :pretty-print false}}]}
