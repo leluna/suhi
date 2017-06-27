c6b01c50f5ecdd38a674cc48964e5f40ec29e79b
leluna <jhelen90@googlemail.com>
2017-06-27 21:10:45 +0200

    fix level dependent speed; slight refactoring

--- suhitetris/src/suhitetris/core.cljs
+++ suhitetris/src/suhitetris/core.cljs
@@ -4,14 +4,22 @@
 
 (enable-console-print!)
 
+
+
+;; global parameters
+
 (def board-height 20)
 (def board-width 10)
 (def starting-pos [0 5])
-(def initial-speed 900)
+(def initial-speed 1000)
+
+
+
+;; tetris artefacts
 
 (def pieces [:z :s :t :l :j :i :o])
 
-;; todo: 0 0 currently has to be in 1st row
+;; 0 0 currently has to be in 1st row
 (def shapes {:t [[0 -1] [0 0] [0 1]
                         [1 0]]
              :s [       [0 0] [0 1]
@@ -26,6 +34,10 @@
              :o [[0 0] [0 1]
                  [1 0] [1 1]]})
 
+
+
+;; default generators for artefacts
+
 (defn random-piece []
   (rand-nth pieces))
 
@@ -35,44 +47,14 @@
 (defn empty-board []
   (into [] (repeat board-height (empty-line))))
 
-(defonce app-state (r/atom {:board (empty-board)
-                            :block {:position starting-pos
-                                    :shape (shapes (random-piece))}
-                            :alive false
-                            :line-clears 0
-                            :score 0
-                            :settings {:gravity true
-                                       :debug false}}))
-
-(defn positions [block]
-  (mapv (partial v/vec+ (:position block)) (:shape block)))
-
-(defn merge-block [board block val]
-    (reduce #(assoc-in %1 %2 val) board (positions block)))
-
-(defn valid? [state]
-  (let [blocks (positions (get-in state [:block]))
-        board  (get-in state [:board])]
-    (every? #(= (get-in board %) 0) blocks)))
-  
-(defn level [line-clears]
-  (min 30 (int (/ line-clears 5))))
-  
-
-;; todo: random rotation + choose position depending on shape
-(defn respawn [state]
-  (let [next-state (-> (assoc-in state [:block :position] starting-pos)
-                       (assoc-in [:block :shape] (shapes (random-piece))))]
-    (if (valid? next-state) next-state
-      (assoc-in state [:alive] false))))
 
 
-(defn line-clears-in [board]
-  (- board-height (count board)))
+;; rules
+(defn level-for [total-line-clears]
+  (min 30 (int (/ total-line-clears 3))))
 
-(defn fill-empty-blocks [board]
-  (into [] (-> (repeat (line-clears-in board) (empty-line))
-               (concat (apply list board)))))
+(defn speed-for [min-speed level]
+  (- min-speed (* level 30)))
 
 (defn score-for [lines level]
   (condp = lines
@@ -82,23 +64,79 @@
     3 (* 300 (+ level 1))
     4 (* 1200 (+ level 1))))
 
+
+
+;; app state
+(defonce app-state (r/atom {:board (empty-board)
+                            :block {:position starting-pos
+                                    :shape (shapes (random-piece))}
+                            :alive false
+                            :line-clears 0
+                            :score 0
+                            :settings {:gravity true
+                                       :debug false}}))
+
+
+
+;; calculation helpers
+
+(defn positions [block]
+  (mapv (partial v/vec+ (:position block)) (:shape block)))
+
+(defn merge-block [board block val]
+    (reduce #(assoc-in %1 %2 val) board (positions block)))
+
+
+;; dependent states
+
+(defn level [state]
+  (level-for (:line-clears state)))
+
+(defn speed [state]
+  (speed-for initial-speed (level state)))
+
+(defn valid? [state]
+  (let [blocks (positions (get-in state [:block]))
+        board  (get-in state [:board])]
+    (every? #(= (get-in board %) 0) blocks)))
+
+
+
+;; actions
+
+;; todo: random rotation + choose position depending on shape
+(defn respawn [state]
+  (let [next-state (-> (assoc-in state [:block :position] starting-pos)
+                       (assoc-in [:block :shape] (shapes (random-piece))))]
+    (if (valid? next-state) next-state
+      (assoc-in state [:alive] false))))
+
+
+(defn- line-clears-in [board]
+  (- board-height (count board)))
+
+(defn- fill-empty-blocks [board]
+  (into [] (-> (repeat (line-clears-in board) (empty-line))
+               (concat (apply list board)))))
+
 (defn update-score [state]
-  (let [board       (get-in state [:board])
-        line-clears (line-clears-in board)
-        level       (level line-clears)]
+  (let [board             (:board state)
+        line-clears       (line-clears-in board)
+        total-line-clears (+ line-clears (:line-clears state))
+        level             (level-for total-line-clears)]
     (-> (update-in state [:score] #(+ % (score-for line-clears level)))
         (update-in [:line-clears] (partial + line-clears)))))
 
 (defn remove-full-lines [board]
   (into [] (remove (partial = (repeat board-width 1)) board)))
-  
 
 (defn kill-block [state]
   (update-in state [:board] #(merge-block % (:block state) 1)))
 
 
 
-;; todo: merge to one function...
+;; interactions
+
 (defn move-down [state]
   (let [next-state (update-in state [:block :position] v/move-down)]
     (if (valid? next-state) next-state
@@ -134,19 +172,21 @@
       (assoc-in [:score] 0)))
 
 
+;; ticker
+
+(defn drop! [] (js/setTimeout 
+                 #(do (when 
+                        (and (:alive @app-state) 
+                             (get-in @app-state [:settings :gravity])) 
+                        (swap! app-state move-down))
+                      (drop!))
+                 (speed @app-state)))
+
+(defonce gravity! (drop!))  
 
 
 
-(defonce drop! (js/setInterval
-                 #(when 
-                    (and (:alive @app-state) 
-                         (get-in @app-state [:settings :gravity])) 
-                    (swap! app-state move-down)) 
-                 (- initial-speed (* (:level @app-state) 10))))  
-
-
-
-
+;; ui
 
 (defn start-overlay [visible resetfn]
   [:div
@@ -177,7 +217,7 @@
      [:div.board
       (map-indexed line-of-blocks (merge-block (:board @app-state) (:block @app-state) 2))
       [score-display (:score @app-state)]
-      [level-display (level (:line-clears @app-state))]
+      [level-display (level @app-state)]
       [start-overlay (not (:alive @app-state)) #(swap! app-state reset)]]]
                            
    (when (get-in @app-state [:settings :debug])
@@ -186,7 +226,9 @@
                :name "Gravity"
                :defaultChecked true
                :on-click #(swap! app-state update-in [:settings :gravity] not)}]
-      [:div (str @app-state)]])]) 
+      [:div (str @app-state)]
+      [:div "level: " (level @app-state)]
+      [:div "speed: " (speed @app-state)]])]) 
 
 
 
