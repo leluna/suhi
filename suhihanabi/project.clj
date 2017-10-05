12bc013b8dc9a6b840060fcc01616f17741b198e
Azel4231 <Azel4231@users.noreply.github.com>
2017-10-05 09:42:13 +0200

    Bump versions, add ring-jetty-adapter

--- suhihanabi/project.clj
+++ suhihanabi/project.clj
@@ -2,10 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.1"]
+                 [ring/ring-jetty-adapter "1.6.2"]]
+  :plugins [[lein-ring "0.12.1"]]
   :ring {:handler suhihanabi.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- suhihanabi/src/suhihanabi/handler.clj
+++ suhihanabi/src/suhihanabi/handler.clj
@@ -1,11 +1,25 @@
 (ns suhihanabi.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.adapter.jetty :refer [run-jetty]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Hallo Welt!")
+  (GET "/lobby" [] "Welcome to the HANABI lobby!")
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [& args]
+  (defonce jetty (run-jetty (var app) {:port 8080 :join? false})))
+
+;;(.stop jetty)
+
+
+;;(-main)
+
+
+
+
