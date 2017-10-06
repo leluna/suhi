7669003445b210658fe0115de22f2ce756d01af4
Azel4231 <Azel4231@users.noreply.github.com>
2017-10-06 09:58:48 +0200

    fix parens

--- suhihanabi/src/suhihanabi/game.clj
+++ suhihanabi/src/suhihanabi/game.clj
@@ -4,34 +4,37 @@
               :green
               :blue
               :yellow
-              :white}
+              :white})
 
-  (def numbers [1 1 1
-                2 2
-                3 3
-                4 4
-                5])
+(def numbers [1 1 1
+              2 2
+              3 3
+              4 4
+              5])
 
-  (def deck (shuffle (for [color colors
-                           number numbers]
-                       {:color color
-                        :number number})))
+(def deck (shuffle (for [color colors
+                         number numbers]
+                     {:color color
+                      :number number})))
 
-  (defonce game-state (atom {:deck deck
-                             :p1  []
-                             :p2  []
-                             :p3  []
-                             :p4  []
-                             :hints 8
-                             :trash []
-                             :storms 0
-                             :table (into {} (for [color colors] [color 0]))}))
+(defonce game-state (atom {:deck deck
+                       :p1  []
+                       :p2  []
+                       :p3  []
+                       :p4  []
+                       :hints 8
+                       :trash []
+                       :storms 0
+                       :table (into {} (for [c colors] [c 0]))
+                       #_(->> colors
+                                   (map #(vector % 0))
+                                   (into {}))
+                       }))
 
+#_(dsui.swing/dsui @game-state)
 
+(defn play [player index])
 
+(defn discard [player index])
 
-  (defn play [player index])
-
-  (defn discard [player index])
-
-  (defn hint-number [from to hint]))
+(defn hint-number [from to hint])
--- suhihanabi/src/suhihanabi/handler.clj
+++ suhihanabi/src/suhihanabi/handler.clj
@@ -17,7 +17,6 @@
 
 ;;(.stop jetty)
 
-
 ;;(-main)
 
 
